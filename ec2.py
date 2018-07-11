d44dc14eb3b216b4f3800a211cb54c55da1156d0
bgdnlp <pawslib.github.com@neant.ro>
2018-07-11 20:30:30 +0300

    Add launch_ami_like_instance()
    Function to launch new instances from an AMI, copying subnet, tags,
    security groups, etc. from an existing EC2 instance.

--- ec2.py
+++ ec2.py
@@ -4,6 +4,99 @@ from ipaddress import ip_network
 import boto3
 
 
+def launch_ami_like_instance(ami_id, model_id, count=1, copy_tags={},
+        b3_session=None, **kwargs):
+    """
+    Start a new instance from the specified AMI, copying the settings
+    (instance type, subnet, security groups, etc.) of another instance.
+
+    Args:
+        ami_id: ID of AMI to use for the new instance
+        model_id: launch new instances in this one's likeness
+        count: number of instances to start
+        copy_tags: dictionary, optional.
+            {
+                'CopyTags': True|False,
+                'SetTags': [
+                    {
+                        'Key': 'string',
+                        'Value': 'string'
+                    }
+                ]
+            }
+            If CopyTags is True, tags present on model instance will
+            also be set on the new ones. Tags present in SetTags will
+            also be set. Tags from SetTags will overwrite the ones
+            coppied from the model instance if they have the same key.
+            If the parameter receives any other values in the
+            dictionary, it will ignore them.
+        b3_session: Boto3 Session object. If passed to the function, boto3
+            clients and resources will be based off it, otherwise the
+            default session will be used.
+        kwargs: any additional parameters will be passed to the boto3
+            function ec2.create_instances() directly. Primary use would
+            probably be to specify ClientToken or PrivateIpAddress.
+            See https://boto3.readthedocs.io/en/latest/reference/services/ec2.html#EC2.ServiceResource.create_instances
+            and https://docs.aws.amazon.com/AWSEC2/latest/APIReference/Run_Instance_Idempotency.html
+            The following parameters can't be used in kwargs:
+                ImageId
+                InstanceType
+                KeyName
+                MaxCount
+                MinCount
+                Monitoring
+                SecurityGroupIds
+                SubnetId
+                EbsOptimized
+                IamInstanceProfile
+
+    Returns:
+        list of instance IDs that are being created
+    """
+    if b3_session:
+        ec2 = b3_session.resource('ec2')
+    else:
+        ec2 = boto3.resource('ec2')
+    ec2_model = ec2.Instance(model_id)
+    # Copy tags
+    if 'SetTags' in copy_tags:
+        tags_to_set = copy_tags['SetTags']
+        tag_keys_to_set = [tag['Key'] for tag in copy_tags['SetTags']]
+        tags_to_copy = [tag for tag in ec2_model.tags 
+                        if tag['Key'] not in tag_keys_to_set]
+    else:
+        tags_to_set = []
+        trags_to_copy = ec2_model.tags
+    if copy_tags['CopyTags']:
+        tag_spec = [{'Tags': tags_to_copy + tags_to_set}]
+    else:
+        tag_spec = [{'Tags': tags_to_set}]
+    tag_spec[0]['ResourceType'] = 'instance'
+    # Not coppied, can be passed on through kwargs
+    #   DisableApiTermination
+    #   InstanceInitiatedShutdownBehavior
+    #   NetworkInterfaces
+    #   InstanceMarketOptions
+    #   CreditSpecification
+    #   CpuOptions
+    print(tag_spec)
+    new_instances = ec2.create_instances(
+        ImageId=ami_id,
+        InstanceType=ec2_model.instance_type,
+        KeyName=ec2_model.key_pair.key_name,
+        MaxCount=count,
+        MinCount=count,
+        Monitoring={'Enabled': ec2_model.monitoring['State'] == 'enabled'},
+        SecurityGroupIds=[gid['GroupId'] for gid in ec2_model.security_groups],
+        SubnetId=ec2_model.subnet_id,
+        EbsOptimized=ec2_model.ebs_optimized,
+        IamInstanceProfile=ec2_model.iam_instance_profile or {},
+        TagSpecifications=tag_spec,
+        **kwargs
+        )
+    return new_instances
+
+
 def split_net_across_zones(net, region, subnets=4, b3_session=None):
     """
     Split a network into subnets across availability zones.
