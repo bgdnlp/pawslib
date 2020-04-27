4f73ae2cc420f5d98f8d83d123c5e3a419bf70e3
bgdnlp <pawslib.github.com@neant.ro>
2020-04-27 10:57:34 +0300

    Set setup.py so it can be installed from GitHub

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="pawslib",
+    version="0.1",
+    author="bgdnlp",
+    author_email="pawslib-github@neant.ro",
+    description="Python Distribution Utilities",
+    url="https://github.com/bgdnlp/pawslib",
+    download_url="https://github.com/bgdnlp/pawslib/archive/master.zip",
+)
